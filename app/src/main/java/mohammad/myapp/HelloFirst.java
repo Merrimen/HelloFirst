0dfd19bb3487de0f62de1c60ec04ec9eb06b8da5
mohammad jazayeri <mohammadr.jazayeri@gmail.com>
2019-02-18 17:15:23 +0330

    Initial commit

---
+++ app/src/main/java/mohammad/myapp/HelloFirst.java
@@ -0,0 +1,13 @@
+package mohammad.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_first);
+    }
+}
